1c9cebe2c2bf43824116d6bf146438d4ea11422f
ethio-dev <eyosiyasabay.contact@gmail.com>
2022-02-18 21:46:38 +0300

    Third Commit

---
+++ Migrations/20220208144133_CreateMigration.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace LoanManagementSystem.Migrations
+{
+    public partial class CreateMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Accounts",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    User_Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    User_Password = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    IsAdmin = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Accounts", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Loan",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FirstName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    LastName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    MiddleName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Sex = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Phone = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    loanPlanId = table.Column<int>(type: "int", nullable: false),
+                    Salary = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    loanTypeId = table.Column<int>(type: "int", nullable: false),
+                    loanPurpose = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    loanAmount = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    LoanGrant = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    loanDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    TotalPayableAmount = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    MonthlyPayableAmount = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    MonthlyPenalty = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    RejectionReason = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    UserId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Loan", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "LoanPlans",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Month = table.Column<int>(type: "int", nullable: false),
+                    Interest = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    MonthlyOverDuePenalty = table.Column<decimal>(type: "decimal(18,2)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_LoanPlans", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "LoanTypes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    LoanTypeName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    LoanDescription = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_LoanTypes", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Payments",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    PayedDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    PayedMonth = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    PayedAmount = table.Column<double>(type: "float", nullable: false),
+                    RemainingLoanAmount = table.Column<double>(type: "float", nullable: false),
+                    RemainingMonthPayment = table.Column<double>(type: "float", nullable: false),
+                    PenaltyPaymentAmount = table.Column<double>(type: "float", nullable: false),
+                    NextPaymentDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    LoanStatus = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    LoanCovered = table.Column<bool>(type: "bit", nullable: false),
+                    UserId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Payments", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Accounts");
+
+            migrationBuilder.DropTable(
+                name: "Loan");
+
+            migrationBuilder.DropTable(
+                name: "LoanPlans");
+
+            migrationBuilder.DropTable(
+                name: "LoanTypes");
+
+            migrationBuilder.DropTable(
+                name: "Payments");
+        }
+    }
+}
---
+++ Migrations/20220216072641_CreateDbTables_.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace LoanManagementSystem.Migrations
+{
+    public partial class CreateDbTables_ : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
